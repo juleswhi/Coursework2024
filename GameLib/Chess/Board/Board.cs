79265227c3e7b6afba120e104587cecd6a8e3a4c
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2023-09-29 00:18:26 +0100

    Delete GameLib directory

--- GameLib/Chess/Board/Board.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chess.Board;
-
-public static class Board
-{
-    // Logic for whose move it is
-    public static bool IsWhiteToMove;
-    public static int MoveColour => IsWhiteToMove ? 1 : 0;
-  
-}
